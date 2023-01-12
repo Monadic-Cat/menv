7d2144ff9e8dac05c91afb2c14f1a7cd59567f99
Monadic Cat <greenmonadcat@gmail.com>
2023-01-12 13:18:25 -0600

    add help generation; predicate to check if any selected vars are set

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,17 @@
 #[macro_export]
 macro_rules! require_envs {
-    ($assert_name:ident; $($fname:ident, $ename:literal, $ty:ty, $etext:literal);* $(;)?) => {
+    (($assert_name:ident, $any_set_name:ident, $help_name:ident); $($fname:ident, $ename:literal, $ty:ty, $etext:literal);* $(;)?) => {
         pub fn $assert_name () {
             // TODO: comment out this body to check if vars are unused
             $(
                 let _ = $fname();
             )*
+        }
+        pub fn $any_set_name() -> bool {
+            [$(::std::env::var($ename)),*].iter().any(|x| x.is_ok())
+        }
+        pub fn $help_name() -> String {
+            String::new() $(+ $etext + "\n")*
         }
         $(
             pub fn $fname() -> $ty {
